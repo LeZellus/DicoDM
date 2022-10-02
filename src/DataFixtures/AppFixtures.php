21051dba92f3afe422c641a0cacebde38ee58b1e
Zelller <matheo.zeller@gmail.com>
2022-10-02 13:24:32 +0200

    :sparkles: add user fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Category;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -21,11 +22,23 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
+        //Create Categories
         $categories = ['Prénom', 'Mot'];
         for ($i = 0; $i <= 1; $i++) {
             $category = new Category();
             $category->setName($categories[$i]);
             $manager->persist($category);
+        }
+
+        $manager->flush();
+
+        //Create Users
+        for ($i = 0; $i <= 49; $i++) {
+            $user = new User();
+            $user->setEmail($this->faker->safeEmail());
+            $user->setPassword($this->faker->password());
+            $user->setRoles(["ROLE_USER"]);
+            $manager->persist($user);
         }
 
         $manager->flush();
