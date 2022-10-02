b79b00f2ffbb77756853db3990873b499e38a0bb
Zelller <matheo.zeller@gmail.com>
2022-10-02 13:15:41 +0200

    :sparkles: add fixtures and faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,14 +5,24 @@ namespace App\DataFixtures;
 use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var Generator|\Faker\Generator
+     */
+    private Generator $faker;
+
+    public function __construct (){
+        $this->faker = Factory::create("fr_FR");
+    }
+
     public function load(ObjectManager $manager): void
     {
         $categories = ['Prénom', 'Mot'];
         for ($i = 0; $i <= 1; $i++) {
-            dump($categories[$i]);
             $category = new Category();
             $category->setName($categories[$i]);
             $manager->persist($category);
