097ee156ea07e4821451b2459115df27ee57972a
Zelller <matheo.zeller@gmail.com>
2022-10-05 16:46:29 +0200

    :bug: entity bug

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -17,7 +17,7 @@ class Comment
     #[ORM\Column(type: Types::TEXT)]
     private ?string $content = null;
 
-    #[ORM\ManyToOne(inversedBy: 'article')]
+    #[ORM\ManyToOne(inversedBy: 'users')]
     private ?User $user = null;
 
     #[ORM\ManyToOne(inversedBy: 'comments')]
